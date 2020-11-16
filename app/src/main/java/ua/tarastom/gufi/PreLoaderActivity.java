8144f575b9d1f0d7306c571ce22167b1074b266e
HeavyMetal77 <ttm2006@ukr.net>
2020-11-16 15:12:39 +0200

    add icon app

--- app/src/main/java/ua/tarastom/gufi/PreLoaderActivity.java
+++ app/src/main/java/ua/tarastom/gufi/PreLoaderActivity.java
@@ -35,4 +35,13 @@ public class PreLoaderActivity extends AppCompatActivity {
         super.onPause();
         finish(); 
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
 }
